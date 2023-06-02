23e25ff929c773fb6553eda125ae40a120f8f017
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-06-02 13:37:34 +0300

    create models and made migrations db

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Gender(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="products_images")
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
