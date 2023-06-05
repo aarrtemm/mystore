4acfa84ad6c931b69a6976e61ac9d5432d95c654
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-06-05 13:32:53 +0300

    Create page products

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from products.views import index
+from products.views import index, ProductListView
 
 app_name = "products"
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("products/", ProductListView.as_view(), name="products")
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from django.views import generic
+
+from products.models import Product, ProductCategory, Gender
 
 
 def index(request, *args, **kwargs):
     return render(request, 'products/index.html')
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = "products/products.html"
+    context_object_name = "products"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductListView, self).get_context_data(**kwargs)
+
+        context["categories"] = ProductCategory.objects.all()
+        context["genders"] = Gender.objects.all()
+
+        return context
