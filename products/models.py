6c5e329029611c34de32b6279c2c0cead81bfe52
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-06-07 16:16:56 +0300

    Deleted related_name to categories

--- products/models.py
+++ products/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    categories = models.ManyToManyField(ProductCategory, related_name="product_categories")
+    categories = models.ManyToManyField(ProductCategory)
     gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
 
     def __str__(self):
